0c26d1194757ed9c4d728821ddbbafc84645a7f7
stivine <ytj1623970771@gmail.com>
2025-05-11 10:52:32 +0800

    test

---
+++ test-rss.js
@@ -0,0 +1,53 @@
+{
+    "camp2025": [
+        {
+            "name": "香港中文大学（深圳）",
+            "institute": "数据科学学院",
+            "description": "第一轮2025年3月1日-2025年4月30日，第二轮2025年5月1日-2025年6月20日",
+            "deadline": "2025-04-30T00:00:00+08:00",
+            "website": "https://sds.cuhk.edu.cn/article/2110",
+            "tags": [
+                "港三"
+            ]
+        },
+        {
+            "name": "中国科学院",
+            "institute": "空天信息创新研究院",
+            "description": "2025年拟招收推免直博/硕士生3名",
+            "deadline": "",
+            "website": "https://mp.weixin.qq.com/s/WXNePcQck_HVdVvOg-fCKg",
+            "tags": [
+                "研究院"
+            ]
+        },
+        {
+            "name": "北京大学",
+            "institute": "前沿交叉学科研究院",
+            "description": "_No response_",
+            "deadline": "2025-06-12T08:00+08:00",
+            "website": "http://www.aais.pku.edu.cn/tongzhi/shownews.php?lang=cn&id=1860",
+            "tags": [
+                "TOP2"
+            ]
+        },
+        {
+            "name": "清华大学",
+            "institute": "统计与数据科学系",
+            "description": "_No response_",
+            "deadline": "2025-04-16T17:00:00+08:00",
+            "website": "https://mp.weixin.qq.com/s/XyUje2eDmgxKKwfP9p5iYw",
+            "tags": [
+                "TOP2"
+            ]
+        },
+        {
+            "name": "北京大学",
+            "institute": "统计科学中心",
+            "description": "_No response_",
+            "deadline": "2025-04-28T00:00:00+08:00",
+            "website": "https://mp.weixin.qq.com/s/B6gyFmMOuvaBKR-dvSJBhA",
+            "tags": [
+                "TOP2"
+            ]
+        },
+}
